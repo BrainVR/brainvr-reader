c3e53fea3ce4eb879a7d08b7bed5389706312f41
hejtmy <hejtmy@gmail.com>
2018-02-01 01:53:12 -0800

    Renames some function to navr generics

--- R/brainvr-getters.R
+++ R/brainvr-getters.R
@@ -1,6 +1,65 @@
-get_trial_event_indices <- function(test, event){
-  indices <- unique(which(test$data$Sender == "Trial" & test$data$Event == event))
-  return(indices + 1)
+#' Title
+#'
+#' @param obj 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_log.brainvr <- function(obj){
+  return(obj$data$player_log)
+}
+
+#' Title
+#'
+#' @param obj 
+#' @param start 
+#' @param end 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_log_timewindow.brainvr <- function(obj, start, end){
+  log <- dt_player[Time > timewindow$start & Time < timewindow$finish, ]
+  return(log)
+}
+
+#' Returns data.table with player log for particular trial
+#'
+#' @param obj
+#' @param 
+#' @return player log for particulat trial
+#' 
+#' @export
+get_trial_log.brainvr <- function(obj, trialId) {
+  timewindow <- get_trial_times.brainvr(obj, trialId)
+  return(get_player_log_timewindow(obj$data$player_log, timewindow$start, timewindow$finish))
+}
+
+#' Gets start and finish times of trial
+#' 
+#' @param obj
+#' @param trialId
+#' @return list with waitingToStart, start and finish 
+#' 
+#' @export
+get_trial_times.brainvr <- function(obj, trialId){
+  df_experiment <- obj$data$experiment_log$data
+  #correction for c# indexing
+  trialId <- trialId - 1
+  
+  ls <- list()
+  df_trial <- df_experiment[df_experiment$Sender == "Trial" & df_experiment$Index == trialId, ]
+  ls$WaitingToStart <-  df_trial[df_trial$Event == "WaitingToStart", "Time"][1]
+  ls$start <- df_trial[df_trial$Event == "Running", "Time"][1]
+  #selects only hte first element - its because fome of hte old logs had potential two finished tiems 
+  #if the experiment or trial was force finished before closed (finished effectively twice)
+  ls$finish <- df_trial[df_trial$Event == "Finished", "Time"][1]
+  #replaces missing values with NAs
+  newValues <- sapply(ls, function(x) if(length(x)== 0) {x <- as.numeric(NA)} else {x <- x})
+  ls <- as.list(newValues)
+  return(ls)
 }
 
 #' Returns pointing direction during given trial. If there are more than two pointings, selects the first one
@@ -29,33 +88,12 @@ get_trial_pointing <- function(obj, trialId, target_pos = NULL){
   return(ls)
 }
 
-#' Gets start and finish times of trial
-#' 
-#' @param obj
-#' @param trialId
-#' @return list with waitingToStart, start and finish 
-#' 
-#' @export
-get_trial_timewindow <- function(obj, trialId){
-  df_experiment <- obj$data$experiment_log$data
-  #correction for c# indexing
-  trialId <- trialId - 1
-  
-  ls <- list()
-  df_trial <- df_experiment[df_experiment$Sender == "Trial" & df_experiment$Index == trialId, ]
-  ls$WaitingToStart <-  df_trial[df_trial$Event == "WaitingToStart", "Time"][1]
-  ls$start <- df_trial[df_trial$Event == "Running", "Time"][1]
-  #selects only hte first element - its because fome of hte old logs had potential two finished tiems 
-  #if the experiment or trial was force finished before closed (finished effectively twice)
-  ls$finish <- df_trial[df_trial$Event == "Finished", "Time"][1]
-  #replaces missing values with NAs
-  newValues <- sapply(ls, function(x) if(length(x)== 0) {x <- as.numeric(NA)} else {x <- x})
-  ls <- as.list(newValues)
-  return(ls)
+get_trial_event_indices <- function(test, event){
+  indices <- unique(which(test$data$Sender == "Trial" & test$data$Event == event))
+  return(indices + 1)
 }
-
 get_walked_distnace_timewindow <- function(dt_position, timeWindow){
-  dt_position <- get_player_log_timewindow(dt_position, timeWindow)
+  dt_position <- get_log_timewindow.brainvr(dt_position, timeWindow$start, timeWindow$finish)
   if (dt_position[, .N] < 2) {
     print("The player log doesn't cover given timewindows")
     walkedDistance <- as.numeric(NA)
@@ -65,24 +103,6 @@ get_walked_distnace_timewindow <- function(dt_position, timeWindow){
     walkedDistance <- end - start
   }
   return(walkedDistance)
-}
-
-#' Returns data.table with player log for particular trial
-#'
-#' @param obj
-#' @param 
-#' @return player log for particulat trial
-#' 
-#' @export
-
-get_trial_log.brainvr <- function(obj, trialId) {
-  timewindow <- get_trial_timewindow(obj, trialId)
-  return(get_player_log_timewindow(obj$data$player_log, timewindow))
-}
-
-get_player_log_timewindow <- function(dt_player, timewindow){
-  log <- dt_player[Time > timewindow$start & Time < timewindow$finish, ]
-  return(log)
 }
 
 was_force_finished <- function(test, trialID){
