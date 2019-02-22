bb8b5330d50ba602ebbff409f0376cc218a5d7b0
hejtmy <hejtmy@gmail.com>
2019-02-22 16:19:41 +0100

    Fixes trimming of timestamps to now keep them at 2 decimal places

--- testing-main.R
+++ testing-main.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(brainvr.R)
 
 dir_path <- "inst/extdata/"
-obj <- load_experiment(dir_path, exp_timestamp = '17-41-52-03-12-2017')
+obj <- load_experiment(dir_path, exp_timestamp = '17-41-52-03-12-2017', override = T)
 obj <- translate_positions(obj, c(33.5, 0, 47.75))
 obj <- mirror_axes(obj)
 obj <- resize_layout(obj, 0.01)
